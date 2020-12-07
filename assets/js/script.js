d4d44ed79103f7ffdea964b3b8f8bf69180b5a8d
Md. Shofiul Alam <67947856+shoalam@users.noreply.github.com>
2020-12-08 02:32:18 +0600

    Adding files for section 2

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+
+
+var modal = document.querySelector('.modal');
+
+// Opening Modal
+
+function openModal(){
+	modal.classList.add('show');
+}
+
+// Closing Modal
+
+function closeModal(){
+	modal.classList.remove('show');
+}
